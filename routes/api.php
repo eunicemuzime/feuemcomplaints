49b7efa19038feeddb5fcae19858f4eb79ecb687
Eunice Muzime <emuzime@localhost.localdomain>
2017-06-24 13:46:40 +0200

    first commit

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,9 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 
 Route::get('/user', function (Request $request) {
@@ -58,3 +57,4 @@ Route::get('/turmas', 'TurmaController@all');
 Route::get('/niveis', 'TurmaController@niveis');
 Route::get('/turnos', 'TurmaController@turnos');
 Route::get('/cursos', 'TurmaController@cursos');
+Route::post('/authenticate', 'AuthenticateController@authenticate');
